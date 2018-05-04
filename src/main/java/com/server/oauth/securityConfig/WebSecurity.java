152932633cebdfd423bde750b8f9e183fd28c4bb
Rahul Mehta <rahulm@unifytech.com>
2018-05-05 02:09:31 +0530

    Auth Server Code

---
+++ src/main/java/com/server/oauth/securityConfig/WebSecurity.java
@@ -0,0 +1,39 @@
+package com.server.oauth.securityConfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("user").roles("USER").and().withUser("bob")
+				.password("abc123").roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("Rahul").password("Pass").roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.anonymous().disable().requestMatchers().antMatchers("/books").and().authorizeRequests()
+				.antMatchers("/newBooks").access("hasRole(USER)").and().exceptionHandling()
+				.accessDeniedHandler(new OAuth2AccessDeniedHandler());
+		http.csrf().disable();
+	}
+
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+}
